bf76e37296eeeea3e76264c00132a4e2d8aa11a0
Nisha Sinha <sinhagautam488@gmail.com>
2026-05-13 16:43:53 +0530

    Initial commit with Docker and CI/CD pipeline

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", process.env.FRONTEND_URL],
     credentials: true,
   })
 );
